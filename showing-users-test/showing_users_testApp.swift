1a2efd1f89d9be64763c784dcc3933cd088bca42
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-07-19 14:37:52 +0200

    Initial Commit

---
+++ showing-users-test/showing_users_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  showing_users_testApp.swift
+//  showing-users-test
+//
+//  Created by Iiro Alhonen on 19.07.21.
+//
+
+import SwiftUI
+
+@main
+struct showing_users_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
